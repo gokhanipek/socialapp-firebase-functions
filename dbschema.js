82701b1524578311c63f60abcb57da448e4d6154
unknown <gokhan.ipek@etiya.com>
2019-08-03 03:27:32 +0300

    'firebase-functions'

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: "2019-08-03T00:18:54.593Z" ,
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
