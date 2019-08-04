b54bcf739cce14979d64f47d0edf8be97c23f2fb
unknown <gokhan.ipek@etiya.com>
2019-08-04 05:50:33 +0300

    'refaktoring-and-users,screams-auth,validation-etc.'

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    apiKey: "AIzaSyCAni_KKGQoYnEA2owamG3iPe-DJsflnHg",
+    authDomain: "socialape-39817.firebaseapp.com",
+    databaseURL: "https://socialape-39817.firebaseio.com",
+    projectId: "socialape-39817",
+    storageBucket: "socialape-39817.appspot.com",
+    messagingSenderId: "911088474454",
+    appId: "1:911088474454:web:62340668fa51c147"
+  };
